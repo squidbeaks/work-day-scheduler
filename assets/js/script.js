d4f2294f0b51b5a1fcfe979bc6196d7f5938f1d4
Leah Russell <leahsigridrussell@gmail.com>
2021-03-07 19:42:47 -0800

    only returning last calendarTime property

--- assets/js/script.js
+++ assets/js/script.js
@@ -67,8 +67,7 @@ timeBlocks.forEach(function(timeBlock, index) {
 
 
 showCurrentDay();
-console.log(calendarTime);
-// showCalendar();
+
 // GIVEN I am using a daily planner to create a schedule
 // WHEN I view the time blocks for that day
 // THEN each time block is color-coded to indicate whether it is in the past, present, or future
